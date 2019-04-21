4af5e80d733abdee9dbb5a35d9e4561aae57c281
Eunsil-Jeong <daramchu@naver.com>
2019-04-21 11:05:24 +0900

    이미지 추가

---
+++ app/src/main/java/com/tj/playstorecopycat/adapters/appAdapter.java
@@ -0,0 +1,9 @@
+package com.tj.playstorecopycat.adapters;
+
+import android.widget.ArrayAdapter;
+
+import com.tj.playstorecopycat.datas.App;
+
+public class appAdapter extends ArrayAdapter<App> {
+
+}
