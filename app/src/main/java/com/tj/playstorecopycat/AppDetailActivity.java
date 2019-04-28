96ba1f247cd280df4ef49e022fc928d98664fa32
Eunsil-Jeong <daramchu@naver.com>
2019-04-28 11:00:50 +0900

    웹페이지 링크 / 스토어 링크 인텐트 활용 예시

--- app/src/main/java/com/tj/playstorecopycat/AppDetailActivity.java
+++ app/src/main/java/com/tj/playstorecopycat/AppDetailActivity.java
@@ -65,6 +65,24 @@ public class AppDetailActivity extends AppCompatActivity {
             }
         });
 
+        act.goHomepageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri homeUri = Uri.parse("https://www.naver.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, homeUri);
+                startActivity(intent);
+            }
+        });
+
+        act.purchaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("market://details?id=com.supercell.brawlstars");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
         act.smsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
